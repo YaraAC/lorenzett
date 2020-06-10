ca21aa47cccce4edf1f7356880b37fc5210a8939
YaraAC <yara.amaral.coutinho@cern.ch>
2020-06-10 21:00:01 +0200

    lulu

---
+++ lulu_reco.sh
@@ -0,0 +1,1 @@
+prun_job.py -c "generator.py --filter Zee -i generator/PythiaGenerator/data/zee_config.cmnd --outputLevel 6 --seed 0 --evt 1 --pileupAvg 0 --bc_id_start -8 --bc_id_end 7" -o def_zee.root -mt 2 -n 1
